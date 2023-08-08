7b6c83f28c8d5fd8ba37a5e2a1381c54f652c31e
Jackie <1824561218@qq.com>
2023-08-08 21:50:37 +0800

    导航

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <!--
  * @Author: Jackie
  * @Date: 2023-06-25 09:58:10
- * @LastEditTime: 2023-08-08 21:20:28
+ * @LastEditTime: 2023-08-08 21:41:47
  * @LastEditors: Jackie
  * @Description: app
  * @FilePath: /Wooden-Fish-Vue-Web/src/App.vue
  * @version: 
 -->
 <template>
-  <router-view />
+  <div id="app">
+    <router-view />
+    <NavBar />
+  </div>
 </template>
-
-<style lang="scss"></style>
+<script setup>
+import NavBar from '@/components/NavBar.vue';
+</script>
+<style lang="scss">
+#app {
+  width: 60%;
+  height: 100vh;
+  /* margin: 0 auto; */
+  position: relative;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,64 @@
+<!--
+ * @Author: Jackie
+ * @Date: 2023-08-07 18:21:45
+ * @LastEditTime: 2023-08-08 21:49:29
+ * @LastEditors: Jackie
+ * @Description: 导航
+ * @FilePath: /Wooden-Fish-Vue-Web/src/components/NavBar.vue
+ * @version: 
+-->
+<template>
+  <div class="navbar">
+    <img src="@/assets/images/muyu.png" class="but" @click="goPath('/')" />
+    <img
+      src="@/assets/images/shaoxiang.png"
+      class="but"
+      @click="goPath('/shaoxiang')"
+    />
+    <img
+      src="@/assets/images/qiuqian.png"
+      class="but"
+      @click="goPath('/qiuqian')"
+    />
+    <img src="@/assets/images/yinyue.png" class="but" @click="music" />
+  </div>
+</template>
+
+<script setup>
+import { onMounted, reactive } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const goPath = (path) => {
+  router.push(path);
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  display: flex;
+  /*水平垂直居中*/
+  align-items: center;
+  justify-content: center;
+
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  .but {
+    width: 50px;
+    height: 120px;
+    margin: 0 30px;
+    cursor: pointer;
+    &:hover {
+      width: 60px;
+      height: 120px;
+      margin: 0 25px;
+    }
+    &:active {
+      width: 50px;
+      height: 120px;
+      margin: 0 30px;
+    }
+  }
+}
+</style>
